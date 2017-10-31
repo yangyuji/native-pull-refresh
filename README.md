f55e740aceb02dda185550cc4a0d02886b03f8ce
yangyuji <394712273@qq.com>
2017-10-31 14:05:49 +0800

    '更新版本'

--- README.md
+++ README.md
@@ -11,11 +11,11 @@
 ## 优点
 > *  代码非常小的库，只有100多行，每个人都能看懂；
 > *  无依赖，这样你就可以用于任何地方了；
-> * 使用简单，只有几个配置项。
+> *  使用简单，只有几个配置项。
 
 ## 缺点
-> *  样式兼容性写法还没补上，如果要用建议参考iscroll.js里面的_prefixStyle方案补全；
-> *  代码兼容性没有做全面的测试，在比较新的safari，QQ浏览器，uc浏览器，以及微信内都正常。
+> *  兼容性还没测试完，在较新的safari，QQ浏览器，uc浏览器，以及微信内都正常；
+> *  暂时没有考虑直接使用body滚动的情况，必须使用固定的dom结构。
 
 ## 说明
 > * style.css是我写的页面样式，仅仅作为demo的展示样式；
@@ -23,11 +23,4 @@
 
 ## 预览
 
-### 普通页面滚动
 ![扫描二维码](https://github.com/yangyuji/naturePullRefresh.js/blob/master/img/qrcode.png)
-
-### 页面带有头部栏
-![扫描二维码](https://github.com/yangyuji/naturePullRefresh.js/blob/master/img/qrcode_has_top.png)
-
-### 页面带有头部栏
-![扫描二维码](https://github.com/yangyuji/naturePullRefresh.js/blob/master/img/qrcode_section.png)
--- naturePullRefresh.js
+++ naturePullRefresh.js
@@ -2,7 +2,7 @@
 * author: "oujizeng",
 * license: "MIT",
 * name: "naturePullRefresh.js",
-* version: "1.1.0"
+* version: "1.2.0"
 */
 
 (function (root, factory) {
@@ -17,8 +17,8 @@
     }
 }(this, function () {
 
-    var getEle = function (id) {
-        return document.getElementById(id);
+    var getEle = function (str) {
+        return document.querySelector(str);
     }, elementDisplay = {};
 
     var util = {
@@ -67,36 +67,39 @@
 
         init: function(opt){
 
-            var dragThreshold = opt.dragThreshold || 0.3,   // 临界值
+            var dragThreshold = opt.dragThreshold || 0.2,   // 临界值
                 moveCount = opt.moveCount || 200,           // 位移系数
+
+                // 执行完需要还原的值
                 dragStart = null,                           // 开始抓取标志位
                 percentage = 0,                             // 拖动量的百分比
                 changeOneTimeFlag = 0,                      // 修改dom只执行1次标志位
-                joinRefreshFlag = null,                     // 进入下拉刷新状态标志位
+                joinRefreshFlag = false,                    // 进入下拉刷新状态标志位
                 refreshFlag = 0;                            // 下拉刷新执行是控制页面假死标志位
 
-            var pullIcon = getEle('pullIcon'),              // 下拉loading
-                pullText = getEle('pullText'),              // 下拉文字
-                pullArrow = getEle('arrowIcon'),            // 下拉箭头
-                pullTop = getEle('pullTop'),                // 拉动的头部
-                dom = getEle(opt.container);                // 主容器
+            var pullIcon = getEle('#pullIcon'),              // 下拉loading
+                pullText = getEle('#pullText'),              // 下拉文字
+                pullArrow = getEle('#arrowIcon'),            // 下拉箭头
+                pullTop = getEle('#pullTop'),                // 拉动的头部
+                container = getEle(opt.container),           // 主容器
+                scroll = container.children[1];
 
-            dom.addEventListener('touchstart', function (event) {
+            container.addEventListener('touchstart', function (event) {
                 if (refreshFlag) {
                     event.preventDefault();
                     return;
                 }
 
-                event = event.touches[0];
-                dragStart = event.clientY;
-                dom.style.transition = 'none';
+                dragStart = event.touches ? event.touches[0].pageY : event.clientY;
+                scroll.style.webkitTransform = '0ms';
+                scroll.style.transition = '0ms';
 
                 util.hide(pullIcon);
                 util.removeClass(pullArrow, 'down');
                 util.removeClass(pullArrow, 'up');
             });
 
-            dom.addEventListener('touchmove', function (event) {
+            container.addEventListener('touchmove', function (event) {
                 if (dragStart === null) {
                     return;
                 }
@@ -104,11 +107,12 @@
                     event.preventDefault();
                     return;
                 }
-                var target = event.touches[0];
-                percentage = (dragStart - target.clientY) / window.screen.height;
+
+                var startY = event.touches ? event.touches[0].pageY : event.clientY;
+                percentage = (dragStart - startY) / window.screen.height;
 
                 // 当scrolltop是0且往下滚动
-                if (dom.scrollTop === 0) {
+                if (container.scrollTop === 0 ) {
                     if (percentage < 0) {
 
                         event.preventDefault();
@@ -132,24 +136,13 @@
                             util.addClass(pullArrow, 'down');
                         }
 
-                        if (percentage > 0) {
-                            dom.style.transform = 'translate3d(0,' + translateX + 'px,0)';
-                        } else {
-                            dom.style.transform = 'translate3d(0,' + translateX + 'px,0)';
-                        }
-                    } else {
-                        if (joinRefreshFlag === null) {
-                            joinRefreshFlag = false;
-                        }
-                    }
-                } else {
-                    if (joinRefreshFlag === null) {
-                        joinRefreshFlag = false;
+                        scroll.style.webkitTransform = 'translate3d(0,' + translateX + 'px,0)';
+                        scroll.style.transform = 'translate3d(0,' + translateX + 'px,0)';
                     }
                 }
             });
 
-            dom.addEventListener('touchend', function(event){
+            container.addEventListener('touchend', function(event){
 
                 if (percentage === 0) {
                     return;
@@ -160,50 +153,49 @@
                     return;
                 }
 
+                // 超过刷新临界值
                 if (Math.abs(percentage) > dragThreshold && joinRefreshFlag) {
 
                     opt.onRefresh && opt.onRefresh();
 
                     util.hide(pullArrow);
-                    dom.style.transition = '330ms';
                     util.show(pullIcon);
                     pullText.textContent = '正在刷新..';
-                    // dom.style.overflow = 'inherit';
-                    dom.style.transform = 'translate3d(0,' + pullTop.clientHeight + 'px,0)';
+                    scroll.style.webkitTransitionDuration = '300ms';
+                    scroll.style.transitionDuration = '300ms';
+                    scroll.style.webkitTransform = 'translate3d(0,' + pullTop.clientHeight + 'px,0)';
+                    scroll.style.transform = 'translate3d(0,' + pullTop.clientHeight + 'px,0)';
 
                     // 进入下拉刷新状态
                     refreshFlag = 1;
                     setTimeout(function () {
-
-                        pullText.textContent = '刷新成功';
-                        dom.style.transform = 'translate3d(0,0,0)';
                         util.hide(pullIcon);
-
-                        setTimeout(function(){
-                            opt.afterPull && opt.afterPull();
-                            refreshFlag = 0;
-                        }, 300);
-
-                    }, 700);
-
-                } else {
-                    if (joinRefreshFlag) {
-                        refreshFlag = 1;
-
-                        dom.style.transition = '330ms';
-                        dom.style.transform = 'translate3d(0,0,0)';
+                        pullText.textContent = '刷新成功';
 
                         setTimeout(function () {
                             opt.afterPull && opt.afterPull();
                             refreshFlag = 0;
+                            scroll.style.webkitTransitionDuration = '300ms';
+                            scroll.style.transitionDuration = '300ms';
+                            scroll.style.webkitTransform = 'translate3d(0,0,0)';
+                            scroll.style.transform = 'translate3d(0,0,0)';
                         }, 300);
+                    }, 300);
+
+                } else {
+                    if (joinRefreshFlag) {
+                        scroll.style.webkitTransitionDuration = '300ms';
+                        scroll.style.transitionDuration = '300ms';
+                        scroll.style.webkitTransform = 'translate3d(0,0,0)';
+                        scroll.style.transform = 'translate3d(0,0,0)';
                     }
                 }
                 // 恢复初始化状态
                 changeOneTimeFlag = 0;
-                joinRefreshFlag = null;
+                joinRefreshFlag = false;
                 dragStart = null;
                 percentage = 0;
+                refreshFlag = 0;
             });
         }
     };
